dbdb56e872813d65996ec196526a9f15239d2b62
Esteban Manchado Velázquez <emanchado@demiurgo.org>
2011-10-30 15:49:43 +0100

    Reorganise init.el, add some stuff

--- editing-functions.el
+++ editing-functions.el
@@ -7,3 +7,31 @@
 
 (fset 'kill-until-start-of-line
    (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ("\260" 0 "%d")) arg)))
+
+(fset 'clone-current-test
+   (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ([24 13 115 101 97 114 99 104 45 98 97 99 107 tab 45 tab return 94 32 32 125 44 return 67108896 14 24 13 115 101 97 114 99 104 45 102 111 114 tab 45 tab return 94 32 32 125 return 1 24 11 25 25] 0 "%d")) arg)))
+
+(defvar isearch-initial-string nil)
+(defun isearch-set-initial-string ()
+  (remove-hook 'isearch-mode-hook 'isearch-set-initial-string)
+  (setq isearch-string isearch-initial-string)
+  (isearch-search-and-update))
+(defun isearch-forward-at-point (&optional regexp-p no-recursive-edit)
+  "Interactive search forward for the symbol at point."
+  (interactive "P\np")
+  (if regexp-p (isearch-forward regexp-p no-recursive-edit)
+    (let* ((end (progn (skip-syntax-forward "w_") (point)))
+           (begin (progn (skip-syntax-backward "w_") (point))))
+      (if (eq begin end)
+          (isearch-forward regexp-p no-recursive-edit)
+        (setq isearch-initial-string (buffer-substring begin end))
+        (add-hook 'isearch-mode-hook 'isearch-set-initial-string)
+        (isearch-forward regexp-p no-recursive-edit)))))
+
+(defun goto-match-paren (arg)
+  "Go to the matching parenthesis if on parenthesis, otherwise insert %.
+vi style of % jumping to matching brace."
+  (interactive "p")
+  (cond ((looking-at "\\s\(") (forward-list 1) (backward-char 1))
+        ((looking-at "\\s\)") (forward-char 1) (backward-list 1))
+        (t (self-insert-command (or arg 1)))))
