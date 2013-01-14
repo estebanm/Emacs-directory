613764d5e40cc3b015f4b1b6e067cb0e3c82e6f4
Esteban Manchado Velázquez <emanchado@demiurgo.org>
2013-01-14 23:58:27 +0100

    Drop C-<tab> as alias for C-x o

--- init.el
+++ init.el
@@ -97,8 +97,6 @@
 (key-chord-define-global "dw" 'kill-following-whitespace)
 ; d0 chord as C-u in bash (mimicking VIM keystrokes)
 (key-chord-define-global "d0" 'kill-until-start-of-line)
-; Change window with Ctrl-TAB
-(global-set-key [\C-tab] 'other-window)
 ; Comment and duplicate line
 (global-set-key (kbd "C-c C-d") 'comment-and-duplicate-line)
 ; Open lines below and above
